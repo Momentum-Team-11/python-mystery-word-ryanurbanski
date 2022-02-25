0ac70d2abdd62d499f803fb56876b4fd5d174c64
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-02-24 21:50:48 -0500

    Reading file in function

--- mystery_word.py
+++ mystery_word.py
@@ -1,6 +1,46 @@
+wordFile = "test-word.txt"              # Single place to change the input file for testing
+
 def play_game():
-    pass
+    get_test_word(wordFile)
+
+
+def get_test_word(file):
+    """Takes a file passed into it and returns a string"""
+    file1 = open(file)
+    strArr = file1.readlines()
+    print(strArr)
+    print(len(strArr))
 
 
 if __name__ == "__main__":
     play_game()
+
+
+
+
+
+
+# GAMEPLAN =============================================
+# Get test word from file
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# F: Get random word from file
+# 
+# 
+# 
+# 
+# # # # # 
+    
+    
