6ed1c8928bd95e79f1fc0036fdac80f0f40b64fa
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-02-24 23:48:37 -0500

    Simple for loop for the win.  Getting a list of locations the character occurs in the sandbox.

--- sandbox.py
+++ sandbox.py
@@ -2,20 +2,50 @@ letter = 'r'
 word = 'breather'
 display = '_ _ _ _ _ _ _'
 
-def switch():
-    # Find locations in breather where r occurs
-    locations = []
-    while word.find(letter) != -1:
+
+
+
+locations = []
+
+i = 0
+while i < len(word):
+    for c in word:
+        if c == letter:
+            locations.append(i)
+            i += 1
+        else:
+            i += 1
         
-        checkPoint = word.find(letter)
-        locations.append(word.find(letter))
 
-        # Perform search from the checkpoint to the end of string
-        # Update checkPoint
-        # Update the locations list
+print(locations)
 
-        print(word.find(letter))
-        print(f'The checkpoint is: {checkPoint}')
-        print(f'The list of locations the letter appears in the word to guess is: {locations}')
 
-switch()
+
+
+
+
+
+# def switch():
+#     # Find locations in breather where r occurs
+#     locations = []
+
+#     print (word[2:8])
+#     print (word[2:8].find('r'))
+
+#     if word[2:len(word)-1].find(letter) != -1:
+        
+#         checkPoint = 0
+#         locations.append(word.find(letter))
+
+#         # Perform search from the checkpoint to the end of string
+#         print(word[2].find(letter))
+
+#         # Update checkPoint
+#         # checkPoint = word.find(letter)
+#         # locations.append(word.find(letter))
+
+#         # Update the locations list
+
+#         print(word.find(letter))
+#         print(f'The checkpoint is: {checkPoint}')
+#         print(f'The list of locations the letter appears in the word to guess is: {locations}')
